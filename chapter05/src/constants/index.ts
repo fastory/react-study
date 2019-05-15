ebf74633173e302df7a98a0c3decbd5b5b269cd1
fastory <hnxw17@163.com>
2019-05-15 19:33:55 +0800

    更新说明

--- chapter05/src/constants/index.ts
+++ chapter05/src/constants/index.ts
@@ -1,5 +1,6 @@
-export const ADD_TODO = 'ADD_TODO';
-export type ADD_TODO = typeof ADD_TODO;  //类型守卫
+export const ADD_TODO = 'ADD_TODO';     //为什么在const 变量以后，还要对这个变量进行类型保护，在是字符串的情况下，这个值是不可以被改变的
+
+export type ADD_TODO = typeof ADD_TODO;  //类型守卫 
 
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 export type SET_VISIBILITY_FILTER = typeof SET_VISIBILITY_FILTER;
